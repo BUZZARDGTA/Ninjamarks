296c9fc0b3aaf5744339d377195ac4c98c9a69c4
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-11-03 11:33:37 +0100

    Delete todo.js

--- todo.js
+++
@@ -1,4 +0,0 @@
-import { scanDuplicates } from "/js/scanDuplicates.js"
-import { scanWebsites } from "/js/scanWebsites.js"
-import { extensionMessageSender } from "../js/extensionMessageSender.js";
-extensionMessageSender("scanWebsites", scanWebsites); // Runs the "scanWebsites" script in the background
